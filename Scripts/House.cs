bfbebc2d66e06c686a3c372b339e01c2629d91ae
Alexander4356 <sasha4356@mail.ru>
2023-02-28 21:40:09 +0300

    Update House.cs

--- Scripts/House.cs
+++ Scripts/House.cs
@@ -7,15 +7,16 @@ public class House : MonoBehaviour
     [SerializeField] private AudioSource _audioSource;
 
     private float _currentVolume = 0f;
-    private float _targetVolume;
     private float _speed = 0.2f;
+    private float _targetVolume;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.TryGetComponent(out ThiefMover mover))
         {
+            _targetVolume = 1;
             _audioSource.Play();
-            StartCoroutine(IncreaseVolume());
+            StartCoroutine(VolumeChange(_targetVolume));
         }
     }
 
@@ -23,36 +24,19 @@ public class House : MonoBehaviour
     {
         if (collision.gameObject.TryGetComponent(out ThiefMover mover))
         {
-            StartCoroutine(LowerVolume());
+            _targetVolume = 0;
+            StartCoroutine(VolumeChange(_targetVolume));
+            _audioSource.Stop();
         }
     }
 
-    private IEnumerator IncreaseVolume()
+    private IEnumerator VolumeChange(float targetVolume)
     {
-        _targetVolume = 1f;
-
-        while (_currentVolume <= _targetVolume)
+        while (_currentVolume != targetVolume)
         {
-            VolumeChange(_targetVolume);
+            _currentVolume = Mathf.MoveTowards(_currentVolume, targetVolume, _speed * Time.deltaTime);
+            _audioSource.volume = _currentVolume;
             yield return null;
         }
-    }
-
-    private IEnumerator LowerVolume()
-    {
-        _targetVolume = 0f;
-
-        while (_currentVolume > 0)
-        {
-            VolumeChange(_targetVolume);
-            yield return null;
-        }
-        _audioSource.Stop();
-    }
-
-    private void VolumeChange(float targetVolume)
-    {
-        _currentVolume = Mathf.MoveTowards(_currentVolume, targetVolume, _speed * Time.deltaTime);
-        _audioSource.volume = _currentVolume;
     }
 }
